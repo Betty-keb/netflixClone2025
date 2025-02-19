467b2deca8b6e2b54cf957fd8b309864bc2ceeea
Betelhem Kebede <betilove6@gmail.com>
2025-02-20 01:09:35 +0300

    last updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// Check if deploying to GitHub Pages
+const isGitHubPages = process.env.NODE_ENV === "production" && process.env.GITHUB_ACTIONS;
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/netflixClone2025/",
+	base: isGitHubPages ? "/netflixClone2025/" : "/", // Use base only for GitHub Pages
 	plugins: [react()],
 });
